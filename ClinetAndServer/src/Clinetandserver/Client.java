1afbf78ba3872c30e388223d6c3bc7e17d08f852
Suleiman Taha <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2019-04-03 22:31:59 +0300

    Client&Server

---
+++ ClinetAndServer/src/Clinetandserver/Client.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clinetandserver;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Toshiba
+ */
+public class Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        // TODO code application logic here
+        System.out.println(" Clinet  class ");
+        Socket socket=new Socket("127.0.0.1",1234); // ip + port numarasi cunku server ile baglamak istiyor eger ayina port numara lazim ki server ile baglariz   
+        ObjectOutputStream objectOutputStream=new ObjectOutputStream(socket.getOutputStream());
+        objectOutputStream.writeObject("Hi I am the clinet ");
+        ObjectInputStream objectInputStream =new ObjectInputStream(socket.getInputStream());
+        System.out.println(objectInputStream.readObject().toString());
+        socket.close();
+    }
+    
+}
---
+++ ClinetAndServer/src/Clinetandserver/Server.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clinetandserver;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Toshiba
+ */
+public class Server {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        System.out.println(" server class ");
+        ServerSocket serversocket = new ServerSocket(1234);//burda gelen port yani  bu port serverin port  clinet baglainma icin kullaniyor  
+        Socket socket =serversocket.accept();// server acma  kanali 
+        System.out.println("clinet is connected ");
+        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());//clinetden gelen mesaji icin almak
+        System.out.println(objectInputStream.readObject().toString());//gelen  mesaji yazdiriyoruz
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());//yazdigimiz mesaj  gonderiyoruz 
+        objectOutputStream.writeObject(" Hi I am the server ");
+       serversocket.close();
+       socket.close();
+    }
+
+   
+}
