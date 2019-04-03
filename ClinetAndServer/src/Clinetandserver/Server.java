4d299801ac5aa489387d0daa3c51113f35198b4b
Suleiman Taha <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2019-04-03 23:05:48 +0300

    Update Server.java

--- ClinetAndServer/src/Clinetandserver/Server.java
+++ ClinetAndServer/src/Clinetandserver/Server.java
@@ -19,21 +19,17 @@ public class Server {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         System.out.println(" server class ");
-        Socket socket;
+        ServerSocket serversocket = new ServerSocket(1234);//burda gelen port yani  bu port serverin port  clinet baglainma icin kullaniyor  
         //   Socket socket=null;
-        // while (!serversocket.isClosed()) {
-        try (ServerSocket serversocket = new ServerSocket(1234) //burda gelen port yani  bu port serverin port  clinet baglainma icin kullaniyor
-        ) {
-            //   Socket socket=null;
-            // while (!serversocket.isClosed()) {
-            socket = serversocket.accept(); // server acma  kanali
-            System.out.println("clinet is connected ");
-            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());//clinetden gelen mesaji icin almak
-            System.out.println(objectInputStream.readObject().toString());//gelen  mesaji yazdiriyoruz
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());//yazdigimiz mesaj  gonderiyoruz 
-            objectOutputStream.writeObject(" Hi I am the server ");
-            //  }
-        } // server acma  kanali
+        // while (!serversocket.isClosed()) {            
+        Socket socket = serversocket.accept();// server acma  kanali 
+        System.out.println("clinet is connected ");
+        ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());//clinetden gelen mesaji icin almak
+        System.out.println(objectInputStream.readObject().toString());//gelen  mesaji yazdiriyoruz
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());//yazdigimiz mesaj  gonderiyoruz 
+        objectOutputStream.writeObject(" Hi I am the server ");
+        //  }
+        serversocket.close();
         socket.close();
     }
 
