f00e5407f5b917490d7873feb611bcce7b7593ec
Suleiman Taha <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2019-04-04 21:38:57 +0300

    thread

--- ClinetAndServer/src/Clinetandserver/Client.java
+++ ClinetAndServer/src/Clinetandserver/Client.java
@@ -8,7 +8,6 @@ package Clinetandserver;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
