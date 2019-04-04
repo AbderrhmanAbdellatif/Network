f0a73afeca7bfe3cefe2be13d544b8fdc4828784
Suleiman Taha <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2019-04-05 00:22:28 +0300

    update

--- ClinetAndServer/src/serverGUI/Server.java
+++ ClinetAndServer/src/serverGUI/Server.java
@@ -9,44 +9,66 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Toshiba
  */
 public class Server {
+    
     public class ListenServerThread extends Thread {
-
+        
         private Server server;
-
+        
         public ListenServerThread(Server server) {
             this.server = server;
         }
-
+        
         @Override
         public void run() {
             while (!this.server.serverSocket.isClosed()) {
-            
+                try {
+                    Socket socket = this.server.serverSocket.accept();
+                    SoketClinetThread clinetThread = new SoketClinetThread(socket);
+                    this.server.soketClinetThreads.add(clinetThread);
+                } catch (IOException ex) {
+                    Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         }
     }
     public ServerSocket serverSocket;//server icin bir soket
     public ArrayList<SoketClinetThread> soketClinetThreads;// gelen clinet listisi
     public ListenServerThread listenServerThread;
-
+    
     public Server(int port) throws IOException {
         serverSocket = new ServerSocket(port);//serve icin  port burada koyuyoruz
         soketClinetThreads = new ArrayList<SoketClinetThread>();//serverde ekleyecegimiz clinetler
     }
-
+    
     public void StopServer() throws IOException {
         this.serverSocket.close();//server kaptmak icin
     }
-
+    
+    public void Startserver() {
+        ListenServerThread listenServerThread = new ListenServerThread(this);
+        listenServerThread.start();
+    }
+    
+    public void SendMessade(Object mesaj, SoketClinetThread clinetThread) {
+        try {
+            clinetThread.sendmesaj(mesaj.toString());
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
     public void SendBrodacastMessade(Object object) throws IOException {
         for (int i = 0; i < soketClinetThreads.size(); i++) {
             soketClinetThreads.get(i).sendmesaj(object.toString());//her clint masaj gonderme 
         }
     }
-
+    
 }
--- ClinetAndServer/src/serverGUI/SoketClinetThread.java
+++ ClinetAndServer/src/serverGUI/SoketClinetThread.java
@@ -37,7 +37,8 @@ public class SoketClinetThread {
                 try {
                     //clinet geldiyse
                     Object mesaj = this.clinetThread.inputStream.readObject();
-                    System.out.println(" Hi " + mesaj.toString());
+                     //System.out.println(" Hi " + mesaj.toString());
+                      ServerGUI.show.setText(" Hi " + mesaj.toString());
                 } catch (IOException ex) {
                     Logger.getLogger(SoketClinetThread.class.getName()).log(Level.SEVERE, null, ex);
                 } catch (ClassNotFoundException ex) {
