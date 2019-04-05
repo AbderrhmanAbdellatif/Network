5e71c5d50ea470c75abd507a09d94b41b5a74ac2
Suleiman Taha <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2019-04-05 18:31:35 +0300

    clinetGUI

---
+++ ClinetAndServer/src/ClinetGUI/Clinet.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ClinetGUI;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Toshiba
+ */
+public class Clinet {
+    
+    public class ListenClientThread extends Thread {
+        
+        private Clinet clinet;
+        
+        public ListenClientThread(Clinet clinet) {
+            this.clinet = clinet;
+        }
+        
+        @Override
+        public void run() {
+            while (socket.isConnected()) {
+                try {
+                    ClinetGUI.dlm.addElement(" Hi i am clinet " + this.clinet.objectInputStream.readObject());
+                } catch (IOException ex) {
+                    Logger.getLogger(Clinet.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(Clinet.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+    
+    private Socket socket;
+    private String ip;
+    private int port;
+    private ObjectOutputStream objectOutputStream;
+    private ObjectInputStream objectInputStream;
+    
+    public Clinet(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+    
+    public void StartClinet() throws IOException {
+      
+            this.socket = new Socket(this.ip, this.port);//clinet ip ve port icin
+            this.objectInputStream = new ObjectInputStream(this.socket.getInputStream());// gelen mesaj 
+            this.objectOutputStream = new ObjectOutputStream(this.socket.getOutputStream());// gonderlien mesaj icin
+            ListenClientThread listenClientThread = new ListenClientThread(this);
+            listenClientThread.start();
+        
+    }
+    
+    public void closeClient() throws IOException {
+        socket.close();
+    }
+    
+    public void MesajGonder(String string) throws IOException {
+        this.objectOutputStream.writeObject(string);
+    }
+    
+}
