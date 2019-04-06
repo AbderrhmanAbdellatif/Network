c2b06f1acb62c9346570a055e1fafe0b4e25b76c
Suleiman Taha <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2019-04-06 17:53:39 +0300

    Update connect4WithoutGUI.java

--- 4Connect/src/connect4/connect4WithoutGUI.java
+++ 4Connect/src/connect4/connect4WithoutGUI.java
@@ -9,8 +9,39 @@ package connect4;
  *
  * @author Toshiba
  */
-
-
 public class connect4WithoutGUI {
-	
+
+    String[][] Bord = {
+        {" ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " "},
+        {" ", " ", " ", " ", " ", " "}
+    };
+
+    boolean control = false;
+
+    public void show_board() {
+        System.out.println("");
+        System.out.println("-------------------------");
+        System.out.println("|    Connect 4  Game    |");
+        System.out.println("-------------------------");
+        for (int i = 0; i < Bord.length; i++) {//satirlar icin 
+            for (int j = 0; j < Bord.length; j++) {//kolumlar icin
+                if (Bord[i][j] == null) {
+                    System.out.print(" | " + " ");
+                } else {
+                    System.out.print("| " + Bord[i][j] + " ");
+                }
+            }
+            System.out.print("|");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        connect4WithoutGUI connect4Game = new connect4WithoutGUI();
+        connect4Game.show_board();
+    }
 }
